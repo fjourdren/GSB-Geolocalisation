663e49dce1fe8eeb0bab4702a8044c19bc0232cb
fjourdren <flavien.jourdren@gmail.com>
2018-04-20 23:39:05 +0200

    Avance geolocalisation

---
+++ GeoLocalisation/app/src/main/java/com/gsb/ppe/geolocalisation/Config.java
@@ -0,0 +1,20 @@
+package com.gsb.ppe.geolocalisation;
+
+public class Config {
+    private static final String API_URL = "http://127.0.0.1";
+    private static final int LOCATION_INTERVAL = 1;
+    private static final float LOCATION_DISTANCE = 0.5f;
+
+
+    public static String getAPI_URL() {
+        return API_URL;
+    }
+
+    public static int getLOCATION_INTERVAL() {
+        return LOCATION_INTERVAL;
+    }
+
+    public static float getLOCATION_DISTANCE() {
+        return LOCATION_DISTANCE;
+    }
+}
